35d85e558cb4a245cbec49ebad4a9072b13086a1
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-02-25 21:15:46 +0100

    button-cards-logic

--- src/components/PickCategory.jsx
+++ src/components/PickCategory.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 import BotanaCard from "./BotanaCard";
+import styles from "./PickCategory.module.css";
+import PlatillosCard from "./PlatillosCard";
 
 export default function PickCategory() {
+  //When button 1 -> Render "botanas" and hide "platillos"
+  const [showBotanas, setShowBotanas] = useState(false);
+  function handleBotanasButton() {
+    setShowBotanas(true);
+    setShowPlatillos(false);
+  }
+
+  //When button 2 -> Render "platillos" and hide "botanas"
+  const [showPlatillos, setShowPlatillos] = useState(false);
+  function handlePlatillosButton() {
+    setShowPlatillos(true);
+    setShowBotanas(false);
+  }
+
   return (
-    <>
+    <div className={styles.pick_cat_container}>
       <h2>Pick a Category</h2>
-      <BotanaCard />
-    </>
+      <div className={styles.buttons}>
+        <button className={styles.button1} onClick={handleBotanasButton}>
+          BOTANAS
+        </button>
+        <button className={styles.button2} onClick={handlePlatillosButton}>
+          PLATILLOS PRINCIPALES
+        </button>
+      </div>
+      <div className={styles.card_container}>
+        {showBotanas && <BotanaCard />}
+        {showPlatillos && <PlatillosCard />}
+      </div>
+    </div>
   );
 }

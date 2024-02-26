e9d5cb28fff6f78324ab7bacc577d4a80bc04774
saeed padkan <s.padkan@gmail.com>
2024-02-26 14:04:00 +0100

    update footer url

--- src/components/Subscriptions.jsx
+++ src/components/Subscriptions.jsx
@@ -1,21 +1,25 @@
-import styles from "../components/Subscriptions.module.css"
+import styles from "../components/Subscriptions.module.css";
 
-export default function Subscriptions(){
-    return (
-        <div className={styles.container} >
-            <div className={styles.subMessage}>
-                <div className={styles.logo}>
-                <img src="./logo.png" alt="logo" />
-                </div>
-                <div className={styles.message} >
-                    <h3>Here we can write something about the company and display the logo next to it Here we can write something about the company and display the logo next to it</h3>
-                </div>
-            </div>
-
-            <div className="styles.subForm" >
-                <input type="email" placeholder="  @ Enter your email"/>
-                <button>Subscribe</button>
-            </div>
+export default function Subscriptions() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.subMessage}>
+        <div className={styles.logo}>
+          <img src="/logo.png" alt="logo" />
         </div>
-    );
+        <div className={styles.message}>
+          <h3>
+            Here we can write something about the company and display the logo
+            next to it Here we can write something about the company and display
+            the logo next to it
+          </h3>
+        </div>
+      </div>
+
+      <div className="styles.subForm">
+        <input type="email" placeholder="  @ Enter your email" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
 }

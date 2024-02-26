0afdd7aaf3a25d7d626ac9dae2717883dd7e202d
Andrea Caviglia <andrea.caviglia@yahoo.com>
2024-02-26 23:23:02 +0100

    done with my part and added herosection for Patricio

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -3,16 +3,24 @@ import styles from "./CategorySection.module.css";
 export default function CategorySection() {
   return (
     <>
-      <h2> Browse by Recipes</h2>
+      <div className={styles.heading}>
+        <h2> Browse by Category</h2>
+      </div>
       <div className={styles.categoryContainer}>
-        <a href="/category">
-          <div className={styles.category} id={styles.category1}></div>
-          <div className={styles.line}>Botanas </div>
+        <a href="/category" className={styles.link}>
+          <div className={styles.categoryWrapper}>
+            <div className={styles.category} id={styles.category1}></div>
+            <div className={styles.line}></div>
+            <div className={styles.name}> Botanas</div>
+          </div>
         </a>
 
-        <a href="/category">
-          <div className={styles.category} id={styles.category2}></div>
-          <div className={styles.line}> Platillos Principales</div>
+        <a href="/category" className={styles.link}>
+          <div className={styles.categoryWrapper}>
+            <div className={styles.category} id={styles.category2}></div>
+            <div className={styles.line}></div>
+            <div className={styles.name}> Platillos Principales</div>
+          </div>
         </a>
       </div>
     </>

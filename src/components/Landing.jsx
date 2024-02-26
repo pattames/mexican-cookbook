8c0b5c1f1e6a4fc5d503d5dc2287a303e7b9e9b5
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-02-26 20:18:32 +0100

    more changes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,13 +7,15 @@
 import HeroSection from "./HeroSection";
 import CategorySection from "./CategorySection";
 import Subscriptions from "./Subscriptions";
+import BrowseByCard from "./BrowseByCard";
 
 export default function Landing() {
   return (
     <>
       <HeroSection />
       <CategorySection />
-      <Subscriptions/>
+      <BrowseByCard />
+      <Subscriptions />
     </>
   );
 }
